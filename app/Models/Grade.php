f79d56409468a1f28defaae3e7c3ca7d556d179f
Edelnits <marycallo0918@gmail.com>
2025-04-03 19:46:39 +0800

    Update Grade.php

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -2,13 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Student;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Grade extends Model
 {
     use HasFactory;
-
+    protected $table = 'grades';
+    protected $fillable =
+    [
+        'student_id',
+        'midterm',
+        'final',
+    ];
     public function student() {
         return $this->belongsTo(Student::class);
     }
