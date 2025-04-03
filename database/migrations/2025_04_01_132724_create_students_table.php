0e36140649ce208db0dc08cd62a7cf8a48d6c178
Edelnits <marycallo0918@gmail.com>
2025-04-03 19:45:07 +0800

    Update 2025_04_01_132724_create_students_table.php

--- database/migrations/2025_04_01_132724_create_students_table.php
+++ database/migrations/2025_04_01_132724_create_students_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -16,11 +17,22 @@ return new class extends Migration
             $table->string('id_number')->unique();
             $table->string('firstname');
             $table->string('middlename')->nullable();
-            $table->string('lastname');
+            $table->string(column: 'lastname');
             $table->foreignId('department_id')->constrained();
             $table->timestamps();
         });
         
+        $students = [
+            ['id_number' => '2021-01-0123', 'firstname' => 'Mary', 'middlename' => 'Maandig', 'lastname' => 'Smith', 'department_id' => 1], 
+            ['id_number' => '2022-02-0323', 'firstname' => 'Edelnits', 'middlename' => 'Paclar', 'lastname' => 'Bugart', 'department_id' => 2],  
+            ['id_number' => '2023-03-0987', 'firstname' => 'Ann', 'middlename' => 'Tokyo', 'lastname' => 'Callo', 'department_id' => 3], 
+        ];
+
+        foreach ($students as $student) {
+            DB::table('students')->insert($student);
+        }
+       
+
     }
 
     /**
