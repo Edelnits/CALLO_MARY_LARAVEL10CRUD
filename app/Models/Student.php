e806b264760f87f3a116ee0e4e8e32c1e32d9152
Edelnits <marycallo0918@gmail.com>
2025-04-03 19:46:56 +0800

    Update Student.php

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,12 +2,24 @@
 
 namespace App\Models;
 
+use App\Models\Department;
+use App\Models\Grade;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
     use HasFactory;
+    protected $table = 'students';
+    protected $fillable =
+    [
+        'id_number',
+        'firstname',
+        'middlename',
+        'lastname',
+        'department_id'
+    ];
+
 
     public function department() {
         return $this->belongsTo(Department::class);
