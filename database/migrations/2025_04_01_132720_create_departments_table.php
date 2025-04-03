bf7d10da0e3ca41dc3e97f48cbf35a7723a01ff8
Edelnits <marycallo0918@gmail.com>
2025-04-03 19:45:40 +0800

    Update 2025_04_01_132720_create_departments_table.php

--- database/migrations/2025_04_01_132720_create_departments_table.php
+++ database/migrations/2025_04_01_132720_create_departments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {
     /**
@@ -16,6 +16,11 @@ return new class extends Migration
             $table->enum('name', ['BSBA', 'EDUCATION', 'BSIT'])->unique();
             $table->timestamps();
         });
+        DB::table('departments')->insert([
+            ['name' => 'BSBA'],
+            ['name' => 'EDUCATION'],
+            ['name' => 'BSIT'],
+        ]);
         
     }
 
